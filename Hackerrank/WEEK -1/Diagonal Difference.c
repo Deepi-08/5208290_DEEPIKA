f26e4e6dad733c18bad3d0c35a56105360732033
Deepi-08 <dpdeepika08@gmail.com>
2025-08-06 18:54:54 +0530

    week1 problems

---
+++ Hackerrank/WEEK -1/Diagonal Difference.c
@@ -0,0 +1,21 @@
+                                     Diagonal Difference
+ 
+
+#include<stdio.h>
+int main(){
+    int n,i,j,diagonal1=0,diagonal2=0;
+    scanf("%d",&n);
+    int arr[n][n];
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+        scanf("%d",&arr[i][j]);
+    if(i == j){
+        diagonal1 += arr[i][j];
+    }
+    if(i+j == n-1){
+         diagonal2 += arr[i][j];
+    }
+}
+    }
+    printf("%d",abs(diagonal1 - diagonal2));
+}
